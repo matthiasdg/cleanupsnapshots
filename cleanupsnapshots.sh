50fb6c414defda6b86d300cf1104359e6fbfb554
Matthias De Geyter <matthias.degeyter@imec.be>
2018-04-25 10:56:49 +0200

    added dry-run option

--- cleanupsnapshots.sh
+++ cleanupsnapshots.sh
@@ -3,7 +3,7 @@ export AWS_ACCESS_KEY_ID=BLABLA
 export AWS_SECRET_ACCESS_KEY=BLABLABLA
 
 function usage() {
-  echo "Usage: ./cleanupsnapshots.sh -d 'lldemo volumes 20??-??-?? 01:00' -t '7 days ago'"
+  echo "Usage: ./cleanupsnapshots.sh -d 'lldemo volumes 20??-??-?? 01:00' -t '7 days ago' [-n]"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -17,6 +17,11 @@ while [[ $# -gt 0 ]]; do
       ;;
     -t | --time)
       TIME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n | --noaction)
+      NOACTION="--dry-run"
       shift # past argument
       shift # past value
       ;;
@@ -35,6 +40,6 @@ EXPIRY_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ" --date "$TIME")
 SNAPSHOT_CANDIDATES=$(aws ec2 describe-snapshots --filters Name=description,Values="$DESCRIPTION" --query 'Snapshots[*].{ID:SnapshotId, Time:StartTime}' --output text --region eu-central-1)
 while read SNAPID SNAPDATE; do
   if [[ "$SNAPDATE" < "$EXPIRY_DATE" ]]; then
-    aws ec2 delete-snapshot --snapshot-id $SNAPID --region eu-central-1
+    aws ec2 delete-snapshot --snapshot-id $SNAPID $NOACTION --region eu-central-1
   fi
 done <<<"$SNAPSHOT_CANDIDATES"
